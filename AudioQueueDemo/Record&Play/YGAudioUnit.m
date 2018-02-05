bc3461c36bf4babf26bd244ff35a3612a1a2887e
wangyaoguo <aysfwyg@163.com>
2018-02-05 15:57:17 +0800

    no message

--- AudioQueueDemo/Record&Play/YGAudioUnit.m
+++ AudioQueueDemo/Record&Play/YGAudioUnit.m
@@ -7,13 +7,18 @@
 //
 
 #import "YGAudioUnit.h"
+#import <AVFoundation/AVFoundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 @implementation YGAudioUnit
 
+static OSStatus MyAURenderCallBack(void                       *inRefCon,
+                                   AudioUnitRenderActionFlags *ioActionFlags,
+                                   const AudioTimeStamp       *inTimeStamp,
+                                   UInt32                     inBusNumber,
+                                   UInt32                     inNumberFrames,
+                                   AudioBufferList            *ioData);
+
 -(void)dd {
-    UInt32 busCount = 2;
-    OSStatus result = AudioUnitSetProperty(mixerUnit, kAudioUnitProperty_ElementCount, kAudioUnitScope_Input, 0, &busCount, sizeof(busCount));
-    
     AudioComponentDescription ioUnitDescription;
     ioUnitDescription.componentType = kAudioUnitType_Output;
     ioUnitDescription.componentSubType = kAudioUnitSubType_RemoteIO;
@@ -21,19 +26,39 @@
     ioUnitDescription.componentFlags = 0;
     ioUnitDescription.componentFlagsMask = 0;
     
+    AudioComponentDescription mixerUnitDescription;
+    
     AUGraph processingGraph;
     NewAUGraph(&processingGraph);
     
     AUNode ioNode;
+    AUNode mixerNode;
+    
     AUGraphAddNode(processingGraph, &ioUnitDescription, &ioNode);
+    AUGraphAddNode(processingGraph, &mixerUnitDescription, &mixerNode);
+    
     AUGraphOpen(processingGraph);
     
-    AudioUnit ioUnit;
-    AUGraphNodeInfo(processingGraph, ioNode, NULL, &ioUnit);
+    //AUGraphNodeInfo(processingGraph, ioNode, NULL, &ioUnit);
+    //AUGraphNodeInfo(processingGraph, mixerNode, NULL, &mixerUnit);
     
-//    AudioComponent foundIoUnitReference = AudioComponentFindNext(NULL, &ioUnitDescription);
-//    AudioUnit ioUnitInstance;
-//    AudioComponentInstanceNew(foundIoUnitReference, &ioUnitInstance);
+    AudioUnit ioUnit;
+    AudioUnit mixerUnit;
+    
+    AudioUnitElement mixerUnitOutputBus = 0;
+    AudioUnitElement ioUnitOutputElement = 0;
+    AudioUnitConnection mixerOutToIoUnitIn;
+    
+    mixerOutToIoUnitIn.sourceAudioUnit = mixerUnit;
+    mixerOutToIoUnitIn.sourceOutputNumber = mixerUnitOutputBus;
+    mixerOutToIoUnitIn.destInputNumber = ioUnitOutputElement;
+    
+    AudioUnitSetProperty(ioUnit, kAudioUnitProperty_MakeConnection, kAudioUnitScope_Input, ioUnitOutputElement, &mixerOutToIoUnitIn, sizeof(mixerOutToIoUnitIn));
+    
+    OSStatus result = AUGraphInitialize(processingGraph);
+    AUGraphStart(processingGraph);
+    AUGraphStop(processingGraph);
+    
 }
 
 @end
