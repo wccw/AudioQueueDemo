a0c06ed4ad3647c1b8d75e14354e523cbe013f2d
wangyaoguo <aysfwyg@163.com>
2018-02-07 14:03:05 +0800

    no message

--- AudioQueueDemo/Record&Play/YGAudioUnit.m
+++ AudioQueueDemo/Record&Play/YGAudioUnit.m
@@ -66,10 +66,14 @@ struct CallbackData {
     
     [sessionInstance setPreferredSampleRate:44100 error:&error];
     if (error) {
-        NSLog(@"set setssion sample rate fail");
+        NSLog(@"set session sample rate fail");
         return;
     }
-    [sessionInstance setActive:YES error:nil];
+    
+    [sessionInstance setActive:YES error:&error];
+    if (error) {
+        NSLog(@"set session active fail");
+    }
 }
 
 
@@ -77,20 +81,20 @@ struct CallbackData {
     NSLog(@"start io unit");
     
     //create a new instance of Remote IO
-    AudioComponentDescription unitDescritpion;
-    unitDescritpion.componentType = kAudioUnitType_Output;
-    unitDescritpion.componentSubType = kAudioUnitSubType_RemoteIO;
-    unitDescritpion.componentManufacturer = kAudioUnitManufacturer_Apple;
-    unitDescritpion.componentFlags = 0;
-    unitDescritpion.componentFlagsMask = 0;
+    AudioComponentDescription componentDesc = {0};
+    componentDesc.componentType = kAudioUnitType_Output;
+    componentDesc.componentSubType = kAudioUnitSubType_RemoteIO;
+    componentDesc.componentManufacturer = kAudioUnitManufacturer_Apple;
+    componentDesc.componentFlags = 0;
+    componentDesc.componentFlagsMask = 0;
     
-    AudioComponent component = AudioComponentFindNext(NULL, &unitDescritpion);
+    AudioComponent component = AudioComponentFindNext(NULL, &componentDesc);
     OSStatus status = AudioComponentInstanceNew(component, &ioUnit);
     if (status != noErr) {
         NSLog(@"set AudioComponentInstanceNew fail");
         return;
     }
-
+    
     //enable IO
     UInt32 one = 1;
     status = AudioUnitSetProperty(ioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, 1, &one, sizeof(one));
@@ -106,29 +110,27 @@ struct CallbackData {
     }
     
     //audio format
+    AudioStreamBasicDescription streamDesc;
+    streamDesc.mSampleRate = 44100;
+    streamDesc.mFormatID = kAudioFormatLinearPCM;
+    streamDesc.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+    streamDesc.mFramesPerPacket = 1;
+    streamDesc.mChannelsPerFrame = 1;
+    streamDesc.mBitsPerChannel = 16;
+    streamDesc.mBytesPerFrame = streamDesc.mBytesPerPacket = 2;
+    streamDesc.mReserved = 0;
     
-    AudioStreamBasicDescription desc = {0};
-    desc.mSampleRate = 44100;
-    desc.mFormatID = kAudioFormatLinearPCM;
-    desc.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
-    desc.mFramesPerPacket = 1;
-    desc.mChannelsPerFrame = 1;
-    desc.mBytesPerFrame = desc.mBytesPerPacket = 16;
-    desc.mReserved = 0;
-    
-    /*
-    status = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &desc, sizeof(desc));
+    status = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &streamDesc, sizeof(streamDesc));
     if (status != noErr) {
         NSLog(@"set audio unit property format fail");
         return;
     }
     
-    status = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &desc, sizeof(desc));
+    status = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &streamDesc, sizeof(streamDesc));
     if (status != noErr) {
         NSLog(@"set audio unit property format fail");
         return;
     }
-     */
     
     UInt32 maxFramesPerSlice = 4096;
     status = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_MaximumFramesPerSlice, kAudioUnitScope_Global, 0, &maxFramesPerSlice, sizeof(UInt32));
@@ -142,6 +144,7 @@ struct CallbackData {
     
     cd.unit = ioUnit;
     
+    /*
     AURenderCallbackStruct renderCallbackStruct;
     renderCallbackStruct.inputProcRefCon = playCallback;
     renderCallbackStruct.inputProcRefCon = NULL;
@@ -150,6 +153,7 @@ struct CallbackData {
         NSLog(@"set render play callback fail");
         return;
     }
+     */
     
     status = AudioUnitInitialize(ioUnit);
     if (status != noErr) {
@@ -166,7 +170,7 @@ static OSStatus playCallback(void                       *inRefCon,
                              UInt32                     inNumberFrames,
                              AudioBufferList            *ioData) {
     
-    AudioUnitRender(cd.unit, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData);
+    //AudioUnitRender(cd.unit, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData);
     return 0;
 }
 
